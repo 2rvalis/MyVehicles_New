187c421a32343c17bdc566c89ded83f988087926
Evangelos <tourvalis@gmail.com>
2026-01-22 13:20:01 +0200

    Remove ConstructedBy annotation from iosMain to fix KSP error

--- composeApp/src/iosMain/kotlin/com/example/myvehicles/RoomConstructor.kt
+++ composeApp/src/iosMain/kotlin/com/example/myvehicles/RoomConstructor.kt
@@ -2,15 +2,14 @@ package com.example.myvehicles
 
 import androidx.room.RoomDatabaseConstructor
 
-// ΠΡΟΣΟΧΗ: Αφαιρέσαμε το @ConstructedBy από εδώ.
-// Υπάρχει ήδη στο commonMain, οπότε εδώ απαγορεύεται.
+// Εδώ ΔΕΝ βάζουμε το @ConstructedBy.
+// Το KSP το βρίσκει αυτόματα από το commonMain.
 @Suppress("NO_ACTUAL_FOR_EXPECT")
 actual object AppDatabaseConstructor : RoomDatabaseConstructor<VehicleDatabase> {
     override fun initialize(): VehicleDatabase = instantiateImpl()
 }
 
 /**
- * Η Room παράγει αυτή τη συνάρτηση αυτόματα στο iOS.
- * Χρησιμοποιούμε 'expect' (αντί για extern) για να είναι συμβατό με τις νέες εκδόσεις.
+ * Αυτή η συνάρτηση παράγεται αυτόματα από τη Room στο iOS build.
  */
 expect fun instantiateImpl(): VehicleDatabase
